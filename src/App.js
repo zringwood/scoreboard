1890771b0059011a172848a2b1577ba196a6af16
Zachary Ringwood <zringwood@gmail.com>
2023-08-21 12:42:58 -0400

    Begun using layout components

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import './App.css';
 import PlayerSection from './components/PlayerSection/PlayerSection';
 import ScoreboardLayout from './components/ScoreboardLayout/ScoreboardLayout';
 import flag from "./assets/flag-japan.webp"
+import flag_netherlands from "./assets/dutch_flag.png"
 function App() {
   return (
+    <ScoreboardLayout>
       <PlayerSection name="Hifume ABE" country = "JPN" flag = {flag} club= "TOKAI UNIVERSITY" ippon = {0} wazari={0} shido = {2}/>
+      <PlayerSection name="Mark HUIZINGA" country = "NDL" flag = {flag_netherlands} club= "HOLLAND JUDO CLUB" ippon = {0} wazari={1} shido = {3}/>
+    </ScoreboardLayout>
   );
 }
 
